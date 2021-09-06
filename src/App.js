78b9ba4280d758f2e401e7380946564263dee3ff
Ankita196 <muskanmakde1999@gmail.com>
2021-09-06 20:45:10 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./style.css";
 
+let curDate = new Date();
+curDate=curDate.getHours();
+let greeting=''
+
+if(curDate>= 1 && curDate < 12) {
+greeting ="Good Morning"
+}else if(curDate>=12 && curDate<19){
+  greeting ="Good AfterNoon"
+}else{
+  greeting ="Good Night"
+}
+
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>{greeting}</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
