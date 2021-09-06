d76cb5aeda0f19f8e2e97bb1236ba63d6a4ae1ca
Ankita196 <muskanmakde1999@gmail.com>
2021-09-06 21:02:27 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./style.css";
 let curDate = new Date();
 curDate=curDate.getHours();
 let greeting=''
-
+let today = new Date().toISOString().slice(0, 10)
+var dateObj = new Date()
+var weekday = dateObj.toLocaleString("default", { weekday: "long" })
 if(curDate>= 1 && curDate < 12) {
 greeting ="Good Morning"
 }else if(curDate>=12 && curDate<19){
@@ -16,8 +18,8 @@ greeting ="Good Morning"
 export default function App() {
   return (
     <div>
-      <h1>{greeting}</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1 >{greeting}{today}</h1>
+     <h1>{weekday}</h1>
     </div>
   );
 }
